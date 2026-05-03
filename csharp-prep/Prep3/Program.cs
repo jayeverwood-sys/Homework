955880d9370f79e812f2b43dbd5a0d363dfab1df
jayeverwood-sys <jayeverwood@gmail.com>
2026-05-02 23:45:58 -0600

    did all three of the assignment in a sort of panic..

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number?: ");
+        int theNumber = int.Parse( Console.ReadLine());
+
+        int guess = -1;
+
+     while (guess != theNumber)
+     {
+        Console.Write("What is your guess?: ");
+        guess = int.Parse(Console.ReadLine());
+
+        if (theNumber > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (theNumber < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("YOU GOT IT!!!");   
+        }
+
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number(0 to quit): ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if(userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int numAdd = 0;
+        foreach (int number in numbers)
+        {
+            numAdd += number;
+
+        }
+        Console.WriteLine($" The sum is: {numAdd}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
