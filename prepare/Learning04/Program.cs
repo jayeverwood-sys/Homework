4c08be3c953fcc18b1ea23fca844942110334f89
jayeverwood-sys <jayeverwood@gmail.com>
2026-06-01 13:23:53 -0600

    Catching up slowly but surely, this was actually pretty fun to cover.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment as1 = new Assignment("Jay Davis", "Calculus");
+        Console.WriteLine(as1.GetSummary());
+
+        MathAssignments as2 = new MathAssignments("Laelle Davis", "Algebra", "2.4","66-70");
+        Console.WriteLine(as2.GetSummary());
+        Console.WriteLine(as2.GetHomeworkList());
+
+        WritingAssignment as3 = new WritingAssignment("Logan Davis", "Creative Writing", "The World of Slay the Spire");
+        Console.WriteLine(as3.GetSummary());
+        Console.WriteLine(as3.GetWritingInfo());
     }
 }
